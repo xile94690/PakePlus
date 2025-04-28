629471115c850130fb87519c7e2c087da5bdb233
xile94690 <xile94690@gmail.com>
2025-04-28 17:00:44 +0800

    update src file from pakeplus

---
+++ src/cateManage-BpRs2yOz.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index-rRFGLrRk.js";const s=t=>a.Post("/admin/categoryList",t),o=t=>a.Post("/admin/categorySave",t),r=t=>a.Post("/admin/categoryDel",t);export{r as D,s as G,o as U};
